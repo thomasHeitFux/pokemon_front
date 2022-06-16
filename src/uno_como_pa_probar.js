a4c13f33f2f33f84905a56e8fdac4262ba7d2447
Leandro Pereyra <70654398+txakurhub@users.noreply.github.com>
2022-06-16 13:11:16 -0300

    Create uno_como_pa_probar.js

---
+++ src/uno_como_pa_probar.js
@@ -0,0 +1,6 @@
+The npx Node.js Package Runner
+https://nodejs.dev › learn › the-npx-nodejs-package-runner
+npx is a very powerful command that's been available in npm starting version 5.2, released in July 2017. If you don't want to install npm, you can install npx as a standalone package. npx lets you run code built with Node.js and published through the npm registry. Easily run local commands
+How to run npx Node.Js command on windows - Stack Overflow
+https://stackoverflow.com › questions › 70009943 › how-to-run-npx-node-js-command-on-windows
+17 nov 2021If you're still seeing the npx isn't recognized, navigate to the nodejs installation, typicall here: C:\Program Files\nodejs You should see a file called npx inside. From here, copy the path and change directory in the console to this location with the following command: cd C:\Program Files\nodejs After that, type the npx commmand: n
