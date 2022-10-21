77254595842044ddd5b12f67f23bccf0e9158c71
thomasHeitFux <thomas41392@gmail.com>
2022-10-21 01:00:22 -0300

    styling

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -57,99 +57,104 @@ export default function Home() {
     return (
 
         <div >
-          {
-          currPokemons.length>0?
-          <div className={style.background}>
+            {
 
-             <div className={style.nav}>
-             <Link to='/'>
-                 <button className={style.back}>Go back</button>
-             </Link>
-             <div className={style.searchCreate}>
-                 <Link to='/create'>
-                     <button className={style.create}>Create</button>
-                 </Link>
-                 <SearchBar />
-             </div>
+                <div className={style.background}>
 
-             <div className={style.filts}>
-                 <select className={style.filters} onChange={e => handlerSort(e)}>
-                     <option value="A-Z">A-Z</option>
-                     <option value="Z_A">Z-A</option>
-                 </select>
+                    <div className={style.nav}>
+                        <Link to='/'>
+                            <button className={style.back}>Go back</button>
+                        </Link>
+                        <div className={style.searchCreate}>
+                            <Link to='/create'>
+                                <button className={style.create}>Create</button>
+                            </Link>
+                            <SearchBar />
+                        </div>
 
-                 {/* botoncito */}
-                 {/* <button onClick={handlerPowerAtack}>atack</button> */}
-                 
-                 <select className={style.filters} onChange={e => handlerOrderPower(e)}>
-                     <option value="strong">Strong</option>
-                     <option value="weak">Weak</option>
-                 </select>
-                 <select className={style.filters} onChange={(e) => handlerCreated(e)}>
-                     <option value="All">All</option>
-                     <option value="existente">existing</option>
-                     <option value="creados">Created</option>
-                 </select>
-                 <select className={style.filters} onChange={e => handleFilterTypes(e)}>
-                     <option value="all">All</option>
-                     <option value="normal">Normal</option>
-                     <option value="fire">Fire</option>
-                     <option value="flying">Flying</option>
-                     <option value="poison">Poison</option>
-                     <option value="rock">Rock</option>
-                     <option value="shadow">Shadow</option>
-                     <option value="dragon">Dragon</option>
-                     <option value="unknown">Unknown</option>
-                     <option value="fairy">Fairy</option>
-                     <option value="dark">Dark</option>
-                     <option value="ice">Ice</option>
-                     <option value="psychic">Psychic</option>
-                     <option value="grass">Grass</option>
-                     <option value="water">Water</option>
-                     <option value="electric">Electric</option>
-                     <option value="steel">Steel</option>
-                     <option value="ghost">Ghost</option>
-                     <option value="bug">Bug</option>
-                     <option value="ground">Ground</option>
-                     <option value="fighting">Fighting</option>
-                 </select>
-             </div>
-         </div>
+                        <div className={style.filts}>
+                            <select className={style.filters} onChange={e => handlerSort(e)}>
+                                <option value="A-Z">A-Z</option>
+                                <option value="Z_A">Z-A</option>
+                            </select>
 
+                            {/* botoncito */}
+                            {/* <button onClick={handlerPowerAtack}>atack</button> */}
 
-         {currPokemons?.map(e => ((
-             <Link to="/home">
-                 <Card
-                     img={e.img}
-                     name={e.name}
-                     type={e.type ? e.type : e.Types.map(e => e.name)}
-                     id={e.id ? e.id : e.idbd}
-                     height={e.height}
-                     weight={e.weight}
-                     atack={e.atack}
-                     defense={e.defense}
-                     hp={e.hp}
-                     speed={e.speed} />
-             </Link>)))}
+                            <select className={style.filters} onChange={e => handlerOrderPower(e)}>
+                                <option value="strong">Strong</option>
+                                <option value="weak">Weak</option>
+                            </select>
+                            <select className={style.filters} onChange={(e) => handlerCreated(e)}>
+                                <option value="All">All</option>
+                                <option value="existente">existing</option>
+                                <option value="creados">Created</option>
+                            </select>
+                            <select className={style.filters} onChange={e => handleFilterTypes(e)}>
+                                <option value="all">All</option>
+                                <option value="normal">Normal</option>
+                                <option value="fire">Fire</option>
+                                <option value="flying">Flying</option>
+                                <option value="poison">Poison</option>
+                                <option value="rock">Rock</option>
+                                <option value="shadow">Shadow</option>
+                                <option value="dragon">Dragon</option>
+                                <option value="unknown">Unknown</option>
+                                <option value="fairy">Fairy</option>
+                                <option value="dark">Dark</option>
+                                <option value="ice">Ice</option>
+                                <option value="psychic">Psychic</option>
+                                <option value="grass">Grass</option>
+                                <option value="water">Water</option>
+                                <option value="electric">Electric</option>
+                                <option value="steel">Steel</option>
+                                <option value="ghost">Ghost</option>
+                                <option value="bug">Bug</option>
+                                <option value="ground">Ground</option>
+                                <option value="fighting">Fighting</option>
+                            </select>
+                        </div>
+                    </div>
 
 
-         <div className={style.pages}>
+                    {currPokemons.length > 0?currPokemons.map(e => ((
+                        
+                            <Link to="/home">
+                                <Card
+                                    img={e.img}
+                                    name={e.name}
+                                    type={e.type ? e.type : e.Types.map(e => e.name)}
+                                    id={e.id ? e.id : e.idbd}
+                                    height={e.height}
+                                    weight={e.weight}
+                                    atack={e.atack}
+                                    defense={e.defense}
+                                    hp={e.hp}
+                                    speed={e.speed} />
+                            </Link>
+                           
+                    )))
+                    : <div><img src='https://i.gifer.com/origin/0d/0dea0c59cbf084d981fc5b55643cb6e6.gif' alt="" /></div>
+                }
 
-             {currentPage > 1 && (<button className={style.prevNext} onClick={() => paginado(currentPage - 1)}>prev</button>)}
 
-             <Paginado pokemonsPerPage={pokemonsPerPage}
-                 allPokemons={allPokemons.length}
-                 paginado={paginado} />
-             
-             {totalPages !== currentPage &&(<button className={style.prevNext} onClick={() => paginado(currentPage + 1)}>next</button>)}
+                    <div className={style.pages}>
+
+                        {currentPage > 1 && (<button className={style.prevNext} onClick={() => paginado(currentPage - 1)}>prev</button>)}
+
+                        <Paginado pokemonsPerPage={pokemonsPerPage}
+                            allPokemons={allPokemons.length}
+                            paginado={paginado} />
+
+                        {totalPages !== currentPage && (<button className={style.prevNext} onClick={() => paginado(currentPage + 1)}>next</button>)}
 
 
-         </div> </div>
-       :<div><img src='https://i.gifer.com/origin/0d/0dea0c59cbf084d981fc5b55643cb6e6.gif' alt="" /></div>
-       } 
-        <div className={style.ash}>
-        <img  src={ash}/>
-        </div>
+                    </div> </div>
+
+            }
+            <div className={style.ash}>
+                <img src={ash} />
+            </div>
 
         </div>
     )
